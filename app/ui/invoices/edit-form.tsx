4f6bd18ade73fec2610d0804080c0ab5ef9cf8f3
yuki627 <xyzopportunity.627@gmail.com>
2026-03-29 09:13:43 +0900

    0329

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { updateInvoice } from '@/app/lib/actions';
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import {
@@ -17,9 +18,10 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   return (
-    <form>
-      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+<form action={updateInvoiceWithId}>
+        <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
